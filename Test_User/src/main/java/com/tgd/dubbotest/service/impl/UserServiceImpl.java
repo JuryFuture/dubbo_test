b1e18cebfc5003270118d8c3866ece084026b243
JuryFuture <juryfuture@gmail.com>
2018-10-31 20:35:56 +0800

    init dubbo test

--- Test_User/src/main/java/com/tgd/dubbotest/service/impl/UserServiceImpl.java
+++ Test_User/src/main/java/com/tgd/dubbotest/service/impl/UserServiceImpl.java
@@ -3,10 +3,8 @@ package com.tgd.dubbotest.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.tgd.dubbotest.entity.user.User;
 import com.tgd.dubbotest.service.UserService;
-import org.springframework.stereotype.Component;
 
 @Service(version = "1.0.0")
-@Component
 public class UserServiceImpl implements UserService {
     @Override
     public User login(String userName, String password) {
